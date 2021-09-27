56b2b2885ac48dd08d05a4f4f115dd7b085e6e16
ridvan dzhalil <fatzey2010@gmail.com>
2021-09-27 10:40:30 +0300

    isPrime calculator changed

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,14 +4,16 @@ import game from '../index.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 1) {
+  if (num < 2) {
     return false;
   }
-  for (let i = 2; i < num; i += 1) {
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
+
   return true;
 };
 
